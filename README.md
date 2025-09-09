b2c9e2336bf8519756cb0d75aade73fffe6b1881
Koichiro Yokoyama <k-yokoyama@esol.co.jp>
2025-09-09 02:27:16 +0000

    Fix Nightly (#719)
    * Bump actions/download-artifact from 1 to 4.1.7 in /.github/workflows
    
    Bumps [actions/download-artifact](https://github.com/actions/download-artifact) from 1 to 4.1.7.
    - [Release notes](https://github.com/actions/download-artifact/releases)
    - [Commits](https://github.com/actions/download-artifact/compare/v1...v4.1.7)
    
    ---
    updated-dependencies:
    - dependency-name: actions/download-artifact
      dependency-type: direct:production
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump upload_artifact
    
    Signed-off-by: Pablo Garrido <pablogs9@gmail.com>
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Signed-off-by: Pablo Garrido <pablogs9@gmail.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- README.md
+++ README.md
@@ -24,7 +24,7 @@ This ROS 2 package is the entry point for building micro-ROS apps for different 
 
 # Supported platforms
 
-This package provides tools and utils to crosscompile micro-ROS with just the common ROS 2 tools for the following platforms platforms.
+This package provides tools and utils to crosscompile micro-ROS with just the common ROS 2 tools for the following platforms.
 
 **Note that this package provides basic support only, intended in particular for ROS developers who are new to microcontrollers. Micro-ROS [components for each platform](#standalone-build-system-tools) are available and provide a deeper and more flexible integration with the platform-specific build systems.**
 
@@ -61,6 +61,7 @@ ros2 run micro_ros_setup component --help
 
 micro-ROS standalone module for specific platforms are:
 
+- a standalone **[micro-ROS component for Renesas e2 studio and RA6M5](https://github.com/micro-ROS/micro_ros_renesas2estudio_component)**: this package enables the integration of micro-ROS in Renesas e2 studio and RA6M5 MCU family.
 - a standalone **[micro-ROS component for ESP-IDF](https://github.com/micro-ROS/micro_ros_espidf_component)**: this package enables the integration of micro-ROS in any Espressif ESP32 IDF project.
 - a standalone **[micro-ROS module for Zephyr RTOS](https://github.com/micro-ROS/micro_ros_zephyr_module)**: this package enables the integration of micro-ROS in any Zephyr RTOS workspace.
 - a standalone **[micro-ROS module for Mbed RTOS](https://github.com/micro-ROS/micro_ros_mbed)**: this package enables the integration of micro-ROS in any Mbed RTOS workspace.
--- config/zenoh/host/create.sh
+++ config/zenoh/host/create.sh
@@ -1,6 +1,8 @@
 # populate the workspace
 mkdir -p src
 
+echo ros2 run micro_ros_setup create_ws.sh src $PREFIX/config/$RTOS/client_ros2_packages.txt $PREFIX/config/$RTOS/$PLATFORM/client_host_packages.repos
+
 ros2 run micro_ros_setup create_ws.sh src $PREFIX/config/$RTOS/client_ros2_packages.txt $PREFIX/config/$RTOS/$PLATFORM/client_host_packages.repos
 
 # add appropriate colcon.meta
@@ -11,13 +13,34 @@ rosdep install -y --from-paths src -i src --skip-keys="$SKIP" -r
 touch src/uros/rclc/rclc_examples/COLCON_IGNORE
 touch src/uros/rclc/rclc_lifecycle/COLCON_IGNORE
 
-# local patches (2024.08.29)
-if [ -d src/uros/zenohpico ] ; then
-    git apply --directory=src/uros/zenohpico \
-	$PREFIX/config/$RTOS/patches/zenohpico/*
+# get rmw_zenoh_pico repository from git or local strage
+if [ ! -v RMW_ZENOH_PICO_PATH ] ; then
+    git clone https://github.com/esol-community/rmw_zenoh_pico -b jazzy src/uros/rmw_zenoh_pico
+else
+    cp -a $RMW_ZENOH_PICO_PATH src/uros/rmw_zenoh_pico
 fi
 
-if [ -d src/uros/rosidl_typesupport_microxrcedds ] ; then
+# copy demo application
+cp -a src/uros/rmw_zenoh_pico/examples/microros/app src/uros/rmw_zenoh_pico/rmw_zenoh_pico/.
+cp -a src/uros/rmw_zenoh_pico/examples/microros/host/* src/uros/rmw_zenoh_pico/rmw_zenoh_pico/app/.
+
+# get zenoh_pico repository from git or local strage
+if [ ! -v ZENOH_PICO_PATH ] ; then
+    git clone https://github.com/eclipse-zenoh/zenoh-pico.git -b 1.4.0 src/uros/zenohpico
+else
+    cp -a $ZENOH_PICO_PATH src/uros/zenohpico
+fi
+
+# local patches (2025.05.25)
+if [ -d src/uros/rmw_zenoh_pico/patches/${ROS_DISTRO}/zenohpico ] && [ ! -v ZENOH_PICO_PATH ] ; then
+    echo apply patch for zenohpico
+    git apply --directory=src/uros/zenohpico \
+	src/uros/rmw_zenoh_pico/patches/${ROS_DISTRO}/zenohpico/* || true
+fi
+
+if [ -d src/uros/rmw_zenoh_pico/patches/${ROS_DISTRO}/rosidl_typesupport_microxrcedds ] ; then
+    echo apply patch for rosidl_typesupport_microxrcedds
     git apply --directory=src/uros/rosidl_typesupport_microxrcedds \
-	$PREFIX/config/$RTOS/patches/rosidl_typesupport_microxrcedds/*
+	src/uros/rmw_zenoh_pico/patches/${ROS_DISTRO}/rosidl_typesupport_microxrcedds/* || true
+    touch src/uros/rosidl_typesupport_microxrcedds/test/COLCON_IGNORE
 fi
